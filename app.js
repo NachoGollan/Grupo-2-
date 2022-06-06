6b54a53295bba7b0bfb349ae766a5de93e007a32
Matiasanon <matias.2k5@gmail.com>
2022-06-06 19:46:26 -0300

    Inicializando proyecto, configurando app.js con express

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const puerto = 3000;
+const publicPath = path.resolve(__dirname, './public');
+
+app.use(express.static(publicPath));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname,'./views/index.html'))
+});
+
+app.listen(puerto,() => {
+    console.log("Servidor corriendo en el puerto " + puerto)
+})
