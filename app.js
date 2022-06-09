4320e0e5ba97049ba24abd7b73b0f4c914ceedae
JefersonChavez88 <fernandostarwars88@gmail.com>
2022-06-09 18:03:24 -0500

    segundo commit

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const express = require('express');
+// todos los requieres, constantes y variables.
 const path = require('path');
+const express = require('express');
 const app = express();
 const puerto = 3000;
-const publicPath = path.resolve(__dirname, './public');
 
+// configuraciones
+const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
+//rutas
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname,'./views/index.html'))
+    res.sendFile(path.join(__dirname,'./views/index.html'));
 });
 
+//levantar nuestro server
 app.listen(puerto,() => {
-    console.log("Servidor corriendo en el puerto " + puerto)
+    console.log("Servidor corriendo en el puerto" + puerto);
 })
