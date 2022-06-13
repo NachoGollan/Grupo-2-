b6ab8c2227dba72ad47591d35574e1e41ebb30f4
MattBrocos1992 <104606411+MattBrocos1992@users.noreply.github.com>
2022-06-13 17:45:31 -0300

    Login/Register subidos

--- app.js
+++ app.js
@@ -6,12 +6,20 @@ const app = express();
 const puerto = 3000;
 
 // configuraciones
-const publicPath = path.resolve(__dirname, './public');
+const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
 //rutas
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname,'./views/index.html'));
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname,"views/index.html"));
+});
+
+app.get("/login", (req,res) => {
+    res.sendFile(path.join(__dirname,"views/login.html"));
+});
+
+app.get("/register", (req,res) => {
+    res.sendFile(path.join(__dirname,"views/register.html"));
 });
 
 //levantar nuestro server
