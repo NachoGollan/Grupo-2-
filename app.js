4a96769433beab3f3d62b60a464ab99bd68d7758
Matiasanon <matias.2k5@gmail.com>
2022-06-14 12:33:53 -0300

    se agregaron las rutas correspondientes al resto de los html

--- app.js
+++ app.js
@@ -7,7 +7,21 @@ const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
 app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname,'./views/index.html'))
+});
+
+app.get('/login', (req,res) => {
+    res.sendFile(path.join(__dirname,'./views/login.html'))
+});
+app.get('/register', (req,res) => {
+    res.sendFile(path.join(__dirname,'./views/register.html'))
+});
+app.get('/productDetails', (req,res) => {
     res.sendFile(path.join(__dirname,'./views/productDetails.html'))
+});
+
+app.get('/productCart', (req,res) => {
+    res.sendFile(path.join(__dirname,'./views/productCart.html'))
 });
 
 app.listen(puerto,() => {
