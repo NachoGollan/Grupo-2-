a898bd89f4c8522ac42d236de1a0aba766473623
Matiasanon <matias.2k5@gmail.com>
2022-06-22 18:41:03 -0300

    eVins

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express'); //
 const path = require('path');
 const app = express();
 const puerto = process.env.PORT || 3000;
-
+const publicPath = path.resolve(__dirname, './public');
 
 app.use(express.static(publicPath));
 
