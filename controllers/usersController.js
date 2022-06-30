4c86a5c3b6e275ac4593a06237d07b6ad75b5777
MaximilianoPola <maxipola95@gmail.com>
2022-06-29 21:08:42 -0300

    Se modificaron las rutas en usersControllers

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,7 +3,7 @@ const path = require ("path")
 
 const usersController = {
     register:(req,res) => {
-        res.render('register', {
+        res.render('/users/register', {
             
         })
     },
