7a20a1bc1c3faafd16b2d683fffd74345f3bb765
MaximilianoPola <maxipola95@gmail.com>
2022-06-30 22:36:12 -0300

    Se modifico la ruta en usersRoutes

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -2,7 +2,7 @@ const express = require ("express")
 const usersController = require("../controllers/usersController")
 const router = express.Router()
 
-router.get('/users/login', usersController.login)
-router.get('/users/register', usersController.register)
+router.get('/login', usersController.login)
+router.get('/register', usersController.register)
 
 module.exports = router
