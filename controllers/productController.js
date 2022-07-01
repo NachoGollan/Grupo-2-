9cc3b802d71dc82b66421c69dee95dc731726821
MaximilianoPola <maxipola95@gmail.com>
2022-06-30 23:07:54 -0300

    Se arreglo la configuracion de productController

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,19 +1,18 @@
-const path = require ("path")
 
 
 const productController = {
     productDetails:(req,res) => {
-        res.render('productDetails', {
+        res.render('product/productDetails', {
             
         })
     },
     productCart:(req,res) => {
-        res.render('/product/productCart', {
+        res.render('product/productCart', {
             
         })
     },
     productCreate:(req,res) => {
-        res.render('productCreate', {
+        res.render('product/productCreate', {
             
         })
     }
