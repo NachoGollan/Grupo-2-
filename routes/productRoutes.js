235a1359277e035a14b9cb63830aadf1db5abe7f
MaximilianoPola <maxipola95@gmail.com>
2022-07-13 00:36:51 -0300

    Se agrego la ruta con metodo post en producRoutes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,6 +4,10 @@ const router = express.Router()
 
 router.get('/productDetails', productController.productDetails)
 router.get('/productCart', productController.productCart)
+
+//CREATE ONE PRODUCTS//
 router.get('/productCreate', productController.productCreate)
+router.post('/', productController.crearProducto)
+
 
 module.exports = router
