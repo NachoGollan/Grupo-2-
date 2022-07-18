afb0277f68d031a46673d599b967c964d5d01c09
Matias Añon <matias.anon@silk-technologies.com>
2022-07-18 17:47:01 -0300

    se corrigio la ruta para product ID

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,13 +2,15 @@ const express = require ("express")
 const router = express.Router()
 const productController = require('../controllers/productController')
 
-router.get('/:id', productController.obtenerProducto)
-router.get('/productDetails', productController.productDetails)
-router.get('/productCart', productController.productCart)
-
 //CREATE ONE PRODUCTS//
 router.get('/productCreate', productController.productCreate)
 router.post('/', productController.crearProducto)
+
+router.get('/productDetails', productController.productDetails)
+router.get('/productCart', productController.productCart)
+router.get('/:id', productController.obtenerProducto)
+
+
 
 
 module.exports = router
