7a8d648558c09af6263a1aadcc6c26cc5f176fa5
MaximilianoPola <maxipola95@gmail.com>
2022-07-17 23:54:05 -0300

    Se creo la ruta al metodo editarProducto

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,9 +6,13 @@ router.get('/:id', productController.obtenerProducto)
 router.get('/productDetails', productController.productDetails)
 router.get('/productCart', productController.productCart)
 
-//CREATE ONE PRODUCTS//
+//CREATE PRODUCTS//
 router.get('/productCreate', productController.productCreate)
 router.post('/', productController.crearProducto)
+
+//EDIT PRODUCTS//
+router.get('/productEdit', productController.editarProducto)
+
 
 
 module.exports = router
