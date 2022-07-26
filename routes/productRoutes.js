e953420b50d76e521cc361a736462b94bbe5b8f2
Matias Añon <matias.anon@silk-technologies.com>
2022-07-25 21:03:20 -0300

    se corrigio ruta a crear producto

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,22 +2,18 @@ const express = require ("express")
 const router = express.Router()
 const productController = require('../controllers/productController')
 
-
 //EDIT PRODUCTS//
-
 router.get('/edit/:idProductoEditable', productController.editarProducto)
 router.put('/edit/:idProductoEditable', productController.actualizarProducto)
 router.delete('/edit/:idProductoEditable', productController.borrarProducto)
 
 
-router.get('/:id', productController.obtenerProducto)
-router.get('/cart', productController.productCart)
-
 //CREATE PRODUCTS//
 router.get('/create', productController.productCreate)
 router.post('/', productController.crearProducto)
 
-
+router.get('/:id', productController.obtenerProducto)
+router.get('/cart', productController.productCart)
 
 
 module.exports = router
