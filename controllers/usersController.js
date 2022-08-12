f43960af0fca668c2474bb6c24c3fc97d4a63c43
Matias Añon <matias.anon@silk-technologies.com>
2022-08-12 01:11:07 -0300

    se creo registro de usuarios y Multer

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,8 @@
+const path = require("path")
+const fs = require('fs')
 
+const productFilePath = path.join(__dirname, '../data/users.json')
+const users = JSON.parse(fs.readFileSync(productFilePath, 'utf-8'))
 
 const usersController = {
     register:(req,res) => {
@@ -10,8 +14,27 @@ const usersController = {
         res.render('users/login', {
             
         })
+    },
+    createUser:(req,res) => {
+        if (req.file) {
+        let data = req.body
+        let newUser =
+        {        
+            id: users[users.length - 1].id + 1,
+            first_name: data.first_name,
+            last_name: data.last_name,
+            image: req.file.filename,
+            email: data.email,
+            password: data.password,
+            birthday: data.birthday,
+        }
+        users.push(newUser)
+            fs.writeFileSync(productFilePath, JSON.stringify(users, null, ' '))
+        res.redirect('/')
+    } else {
+        res.render('users/register')
     }
-
+    }
 }
 
-module.exports = usersController
+module.exports = usersController 
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,8 +1,25 @@
 const express = require ("express")
 const usersController = require("../controllers/usersController")
+const multer = require('multer')
 const router = express.Router()
+const path = require('path')
+
+const storage = multer.diskStorage({
+
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '../public/images/profile_images'))
+    },
+    filename: (req, file, cb) => {
+        const newFileName = 'user-' + Date.now() + path.extname(file.originalname);
+        cb(null, newFileName )
+    }
+})
+
+const upload = multer({ storage })
 
 router.get('/login', usersController.login)
 router.get('/register', usersController.register)
+
+router.post('/', upload.single('image'), usersController.createUser)
 
 module.exports = router
