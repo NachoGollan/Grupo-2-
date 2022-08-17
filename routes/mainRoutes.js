7296a3adc12af2170db3032fc55a4b5b4a41ea1e
MaximilianoPola <maxipola95@gmail.com>
2022-08-17 00:10:46 -0300

    Se arreglo error de ortografico

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -2,6 +2,7 @@ const express = require ("express")
 const mainController = require("../controllers/mainController")
 const router = express.Router()
 
+
 router.get('/', mainController.index)
 router.get('/menu', mainController.menu)
 
