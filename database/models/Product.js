e61479d86e57f9ed74650f7be0e1a7f1c5fe48ad
MaximilianoPola <maxipola95@gmail.com>
2022-08-30 18:20:48 -0300

    Se corrigio el indentado de las columnas

--- database/models/Product.js
+++ database/models/Product.js
@@ -3,39 +3,39 @@ module.exports = (sequlize, dataTypes) => {
     let alias = "Products"
     let cols = {
         product_id:{
-                    type: dataTypes.INTEGER,
-                    primaryKey: true,
-                    autoIncrement: true
-                    },
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
         product_name:{
-                    type: dataTypes.STRING
-                    },
+            type: dataTypes.STRING
+        },
         descript:{
-                    type: dataTypes.STRING
-                },
+            type: dataTypes.STRING
+        },
         category_id:{
-                type: dataTypes.INTEGER,
-                foreignKey: true
-                },
+            type: dataTypes.INTEGER,
+            foreignKey: true
+        },
         brand_id:{
-                type: dataTypes.INTEGER,
-                foreignKey: true
-                },
+            type: dataTypes.INTEGER,
+            foreignKey: true
+        },
         offer:{
-                type: dataTypes.BOOLEAN
-                },
+            type: dataTypes.BOOLEAN
+        },
         discount: {
-                type: dataTypes.DECIMAL
-                },
+            type: dataTypes.DECIMAL
+        },
         price:{
             type: dataTypes.DECIMAL
-            },
+        },
         imported:{
             type: dataTypes.BOOLEAN
-            },
+        },
         image:{
             type: dataTypes.DECIMAL
-            }
+        }
     }
     let config = {
         tableName: "products",
