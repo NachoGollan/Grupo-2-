3ca3abd0dd5faeae2c66266e2d2925d24b6f06f2
MaximilianoPola <maxipola95@gmail.com>
2022-08-30 18:19:53 -0300

    Se creo el modelo User

---
+++ database/models/User.js
@@ -0,0 +1,42 @@
+module.exports = (sequlize, dataTypes) => {
+    let alias = "Users"
+    let cols = {
+        users_id:{
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+            },
+        user_name:{
+            type: dataTypes.STRING
+            },
+        first_name:{
+            type: dataTypes.STRING
+            },
+        last_name:{
+            type: dataTypes.STRING,
+            },
+        email:{
+            type: dataTypes.STRING,
+            unique: true,
+            },
+        passwd:{
+            type: dataTypes.STRING
+            },
+        birthday: {
+            type: dataTypes.DATE
+            },
+        image:{
+            type: dataTypes.BLOB
+            },
+        role_id:{
+            type: dataTypes.INTEGER,
+            foreignKey: true
+            }
+    }
+    let config = {
+        tableName: "users",
+        timestapms: false
+    }
+    const Usuario = sequlize.define(alias, cols, config)
+
+    return Usuario
