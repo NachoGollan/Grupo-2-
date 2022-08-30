2e761ba2398eda92319037e387d3cd12b4674ed4
MaximilianoPola <maxipola95@gmail.com>
2022-08-30 18:22:01 -0300

    Se corrigio el indentado del modelo  User

--- database/models/User.js
+++ database/models/User.js
@@ -5,33 +5,33 @@ module.exports = (sequlize, dataTypes) => {
             type: dataTypes.INTEGER,
             primaryKey: true,
             autoIncrement: true
-            },
+        },
         user_name:{
             type: dataTypes.STRING
-            },
+        },
         first_name:{
             type: dataTypes.STRING
-            },
+        },
         last_name:{
             type: dataTypes.STRING,
-            },
+        },
         email:{
             type: dataTypes.STRING,
             unique: true,
-            },
+        },
         passwd:{
             type: dataTypes.STRING
-            },
+        },
         birthday: {
             type: dataTypes.DATE
-            },
+        },
         image:{
             type: dataTypes.BLOB
-            },
+        },
         role_id:{
             type: dataTypes.INTEGER,
             foreignKey: true
-            }
+        }
     }
     let config = {
         tableName: "users",
@@ -40,3 +40,4 @@ module.exports = (sequlize, dataTypes) => {
     const Usuario = sequlize.define(alias, cols, config)
 
     return Usuario
+}
