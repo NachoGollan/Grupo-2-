77939039e128beccb58f169c29e3e4bb45e5ffa4
MaximilianoPola <maxipola95@gmail.com>
2022-09-09 21:17:08 -0300

    Se agrego multer a las rutas de productos

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,22 @@
 const express = require ("express")
 const router = express.Router()
 const productController = require('../controllers/productController')
+const multer = require('multer')
+const path = require('path')
+
+const storage = multer.diskStorage({
+
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '../public/images/vinos'))
+    },
+    filename: (req, file, cb) => {
+        const newFileName = 'product-' + Date.now() + path.extname(file.originalname);
+        cb(null, newFileName )
+    }
+})
+const upload = multer({ storage })
+
+
 
 //EDIT PRODUCTS//
 router.get('/edit/:idProductoEditable', productController.editarProducto)
