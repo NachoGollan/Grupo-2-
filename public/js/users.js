956f151eb7da67ddcb7138d70231eefc3f28779c
MaximilianoPola <maxipola95@gmail.com>
2022-09-20 17:44:38 -0300

    Se agrego estilo a los errores

--- public/js/users.js
+++ public/js/users.js
@@ -7,18 +7,18 @@ let passwordLogin = document.querySelector ('.input-password')
 let erroresLogin = document.querySelector("div.errores-login ul")
 let errores = []
 
-    form.addEventListener('submit', (e) => {
-        
-        if(nameLogin.value == ""){
-            errores.push("Por favor ingrese su email")
-        }
-
-        if(passwordLogin.value == ""){
-            errores.push("Por favor ingrese su contraseña")
-        }
-        
-        if(errores.length > 0){ 
-            e.preventDefault()
+form.addEventListener('submit', (e) => {
+    
+    if(nameLogin.value == ""){
+        errores.push("Por favor ingrese su email")
+    }
+    
+    if(passwordLogin.value == ""){
+        errores.push("Por favor ingrese su contraseña")
+    }
+    
+    if(errores.length > 0){ 
+        e.preventDefault()
             for(let i = 0; i < errores.length; i++){
                 erroresLogin.innerHTML += "<li>" + errores[i] + "</li>"
                 
