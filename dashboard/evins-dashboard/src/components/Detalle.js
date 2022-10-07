624ea670a470e347cbe672854ac3f430901c8d4b
MaximilianoPola <maxipola95@gmail.com>
2022-10-06 23:29:24 -0300

    Se creo el componente Detalle y componente Panel

---
+++ dashboard/evins-dashboard/src/components/Detalle.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+function Detalle (props){
+    return (
+        <div>
+            <p>Hola</p>
+        </div>
+    )
+}
+
+export default Detalle
---
+++ dashboard/evins-dashboard/src/components/Panel.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+function Panel (props){
+    return (
+        <div>
+            <p>Hola,</p>
+        </div>
+    )
+}
+
+export default Panel
