da7c639c229807e08e9799c2219eb90ecf077cc7
MaximilianoPola <maxipola95@gmail.com>
2022-10-07 17:09:56 -0300

    Creacion props para componenete panel

--- dashboard/evins-dashboard/src/components/Panel.js
+++ dashboard/evins-dashboard/src/components/Panel.js
@@ -3,9 +3,11 @@ import React, {Component} from "react";
 function Panel (props){
     return (
         <div>
-            <p>Hola,</p>
+            <ul>
+                {props.totales.map(total => <li>{total}</li>)}
+            </ul>
         </div>
-    )
+    ) 
 }
 
 export default Panel
