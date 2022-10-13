e027cea244ff6dd43e944c435c9279f879b066d9
Matiasanon <matias.2k5@gmail.com>
2022-10-13 17:32:45 -0300

    se configuraron imagenes de productos desde db

--- dashboard/evins-dashboard/src/components/ChartRow.js
+++ dashboard/evins-dashboard/src/components/ChartRow.js
@@ -7,7 +7,7 @@ function ChartRow(props){
                     <td>{props.product_name}</td>
                     <td>{props.price}</td>
                     <td>{props.category_id}</td>
-                    <td>{props.offer}%</td>
+                    <td>{props.discount}%</td>
                     <td> <a href = {`http://localhost:3001/product/${props.product_id}`}>{props.product_id}</a> </td>
                 </tr>
     )
